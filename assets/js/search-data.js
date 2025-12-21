398dcc8cb7eafd21a2e47700ff25af240fdde9a3
micaelaroth <55409401+micaelaroth@users.noreply.github.com>
2025-12-21 06:23:47 +0000

    Deploying to gh-pages from @ micaelaroth/micaelaroth.github.io@75ab000d0167ee97556084ce818955340fd88f3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-math-notes",
+          title: "math notes",
           description: "",
           section: "Navigation",
           handler: () => {
